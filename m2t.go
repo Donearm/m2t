ccf3c5fb424077d432a69411fcf08f68f28088f3
Gianluca Fiore <forod.g@gmail.com>
2016-06-16 10:45:01 +0200

    v0.1: working, alpha, state

--- m2t.go
+++ m2t.go
@@ -14,7 +14,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path"
 	"regexp"
+	"strconv"
 )
 
 var mgnt_type string
@@ -24,8 +26,8 @@ var watchdir string
 func init() {
 
 	const (
-		def_watchdir	= "/media/private/torrent/watch"
-		def_type		= "others"
+		def_watchdir = "/media/private/torrent/watch"
+		def_type     = "others"
 	)
 
 	flag.StringVar(&mgnt_type, "type", def_type, "")
@@ -36,7 +38,7 @@ func init() {
 }
 
 func main() {
-	var torrentFilename string
+	var torrentFilename, fileName string
 
 	// initialize cli arguments
 	flag.Parse()
@@ -47,25 +49,44 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Compose the watch dir's path for the torrent file
+	outputDir := path.Join(watchdir, mgnt_type)
+	// Reassign first flag to a variable
+	magnetLink := flag.Arg(0)
+
 	// Compile a couple of regexp we need
 	var validMagnet = regexp.MustCompile(`xt=urn:btih:([^&/]+)`)
 	var displayName = regexp.MustCompile(`dn=([^&/]+)`)
 
-	if validMagnet.MatchString(flag.Arg(0)) {
-		if displayName.MatchString(flag.Arg(0)) {
-			torrentFilename = displayName.FindString(flag.Arg(0))
+	if validMagnet.MatchString(magnetLink) {
+		if displayName.MatchString(magnetLink) {
+			torrentFilename = displayName.FindString(magnetLink)
 			// split at '='
-			fileName := regexp.MustCompile(`=`).Split(torrentFilename, -1)[1]
-			if (len(fileName) == 0) {
-				xt := validMagnet.FindString(flag.Arg(0))
+			fileName = regexp.MustCompile(`=`).Split(torrentFilename, -1)[1]
+			if len(fileName) == 0 {
+				xt := validMagnet.FindString(magnetLink)
 				fileName = regexp.MustCompile(`:`).Split(xt, -1)[2]
 			}
-			fmt.Println(torrentFilename)
-			fmt.Println(fileName)
+			// Add torrent extension
+			fileName = fileName + ".torrent"
 		}
 	} else {
 		// not a valid magnet URI given
 		fmt.Println("The magnet URI is not correct or unparseable")
 		os.Exit(1)
 	}
+
+	// Create torrent file and output magnet link string to it
+	f, err := os.Create(path.Join(outputDir, fileName))
+	if err != nil {
+		panic(err)
+	}
+	_, w_err := f.WriteString("d10:magnet-uri" + strconv.Itoa(len(magnetLink)) + ":" + magnetLink + "e")
+	if w_err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	os.Exit(0)
 }
